ecd1033d4712aebcebed070112a9a5478ca02039
Karissa McKelvey <krmckelv@gmail.com>
2019-01-09 09:13:19 -0800

    use tilelive-browser package

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 global.setImmediate = require('timers').setImmediate // monkey patch for tilelive-http
 const path = require('path')
-const tilelive = require('tilelive-streaming')(require('@mapbox/tilelive'))
-const StreamSaver = require('streamsaver')
+const tilelive = require('tilelive-streaming')(require('tilelive-browser'))
+const store = require('browser-cache-blob-store')
+const pump = require('pump')
+
 require('tilelive-http')(tilelive)
 require('@mapbox/tilejson').registerProtocols(tilelive)
 require('tilelive-tar').registerProtocols(tilelive)
@@ -10,27 +12,42 @@ require('tilelive-tar').registerProtocols(tilelive)
 * Download tile data given a query.
 */
 
+var blobStore = store()
+let browser = window.browser
+
 module.exports = function (url, data, cb) {
   tilelive.load(url, function (err, source) {
     if (err) throw err
     var sinkUrl = 'tar://tiles' + path.extname(url)
     tilelive.load(sinkUrl, function (err, sink) {
       if (err) throw err
-      var fileStream = StreamSaver.createWriteStream('tiles.tar')
-      var writer = fileStream.getWriter()
-      sink.pack.on('data', function (data) {
-        writer.write(data)
+      var ws = blobStore.createWriteStream('tiles.tar', () => {
+        var filename = 'tiles.tar'
+        console.log('downloading', filename)
+        browser.downloads.download({
+          url: `/export/${filename}`,
+          filename: filename,
+          conflictAction: 'uniquify'
+        })
       })
+
+      sink.pack.on('data', function (data) {
+        ws.write(data)
+      })
+
       var bounds = [data.minLng, data.minLat, data.maxLng, data.maxLat]
-      var stream = source.createReadStream({
+      var reader = source.createReadStream({
         minzoom: data.minZoom,
         maxzoom: data.maxZoom || data.minZoom + 1,
         bounds: bounds
-      }).pipe(sink.createWriteStream())
-
-      stream.on('end', function () {
-        writer.close()
       })
+      var writer = sink.createWriteStream()
+
+      var stream = pump(reader, writer, function (err) {
+        if (err) console.error(err)
+        ws.close()
+      })
+
       cb(stream)
     })
   })
